42103809238e7aece776655fa721d824df874136
bsinoimeri <b.sinoimeri@hotmail.com>
2020-02-23 19:11:53 +0100

    The Continuous Deployment worked

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Our Continuous Deployment works !
+          Our Continuous Deployment works
         </p>
         <a
           className="App-link"
